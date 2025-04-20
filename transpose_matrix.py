2fc0735928562aa6e4827b36c932478889007ca5
Priya Dave <priyadave300@gmail.com>
2025-04-19 19:29:18 -0700

    Update transpose_matrix.py

--- transpose_matrix.py
+++ transpose_matrix.py
@@ -1,4 +1,3 @@
-# Write your solution here
 ##here we can 2 ways transpose one is by creating another matrix by copying all elements of old mtatrix
 ##then just assign temp[i][j] with matrix[j][i] by transversing through for loop for row and col
 def transpose1(matrix: list):
@@ -19,9 +18,6 @@ def transpose(matrix: list):
             matrix[i][j]= matrix[j][i]
             matrix[j][i] = temp_var
     print(matrix)
-
-
-
 
 if __name__ == "__main__":
     matrix = [[1,2,3],[4,5,6],[7,8,9]]
